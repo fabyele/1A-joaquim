bd56ee307bcc91f7a2ca84927d6c95827e6f4754
fabyele <162468954+fabyele@users.noreply.github.com>
2024-10-24 08:26:49 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,31 +1,53 @@
-//clube da luta, 18, ação, suspense
-//a hora da estrela, 12, drama, comédia 
-//as aventuras do ursinho pooh, livre, infantil, musical
-//500 dias com ela, 12, comédia, romance
-//cidade de deus, 16, crime, drama
-
 let campoIdade;
+let campoAcao;
+let campoSuspense;
 
 function setup() {
-  createCanvas(400, 400);
-  campoIdade = createInput("5");
+  createCanvas(800, 400);
+  createElement("h2", "Recomendador de filmes");
+  createSpan("Sua idade:");
+  campoIdade = createInput("14");
+  campoAcao = createCheckbox("Gosta de ação?");
+  campoSuspense = createCheckbox("Gosta de suspense?");
 }
 
 function draw() {
-  background(220);
+  background("white");
   let idade = campoIdade.value();
-  let recomendacao = geraRecomendacao(idade);
-  text(recomendacao, width / 2, height / 2);
+  let gostaDeAcao = campoAcao.checked();
+  let gostaDeSuspense = campoSuspense.checked();
+  let Recomendacao = geraRecomendacao(idade, gostaDeAcao, gostaDeSuspense);
+
+  fill(color(76, 0, 115));
+  textAlign(CENTER, CENTER);
+  textSize(38);
+  text(Recomendacao, width / 2, height / 2);
 }
 
-function geraRecomendacao(idade) {
-  if(idade >= 16) {
-    if(idade >= 10) {
-      return "cidade de deus";
+function geraRecomendacao(idade, gostaDeAção, gostaDeSuspense) {
+  if (idade >= 12) {
+    if (idade >= 18) {
+      return "Clube da luta";
     } else {
-      return "as aventuras do ursinho pooh";
+      if (idade >= 16) {
+        if(gostaDeAção || gostaDeSuspense) {
+          return "estômago";          
+        } else{
+         return "bicho de sete cabeças";
+        }
+      } else {
+        if (gostaDeAção) {
+          return "estômago";
+        } else {
+          return "O brilho eterno de uma mente sem lembranças";
+        }
+      }
     }
   } else {
-    return "clube da luta";
+    if (gostaDeSuspense) {
+      return "brilho eterno de uma mente sem lembrança";
+    } else {
+      return "tropa de elite";
+    }
   }
 }
